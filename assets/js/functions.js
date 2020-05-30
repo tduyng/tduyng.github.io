df523a27cc80c89c54dbdf077d06a187e522ae98
Tien Duy NGUYEN <39387827+tienduy-nguyen@users.noreply.github.com>
2020-05-30 09:35:56 +0200

    Fix problem smooth scrolling effect

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,18 +7,3 @@ $(
 ).on('click', function () {
   $('html').toggleClass('nav-open');
 });
-//Smooth Scroll
-// $(function() {
-//   $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 500);
-//         return false;
-//       }
-//     }
-//   });
-// });
