e6fffa7025b209586c5a94f7037b4919f0209cae
Tien Duy NGUYEN <39387827+tienduy-nguyen@users.noreply.github.com>
2020-05-30 03:00:55 +0200

    Remove smooth scroll of jquery

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,22 +1,24 @@
-$('.toggled-nav-wrapper').on('click', function(e){
-    e.stopPropagation();
-  });
+$('.toggled-nav-wrapper').on('click', function (e) {
+  e.stopPropagation();
+});
 
-$('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
+$(
+  '.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay'
+).on('click', function () {
   $('html').toggleClass('nav-open');
 });
 //Smooth Scroll
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
-  });
-});
+// $(function() {
+//   $('a[href*="#"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         }, 500);
+//         return false;
+//       }
+//     }
+//   });
+// });
