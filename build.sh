69e2caa444df06a925b52adf631621f5efaeb338
tduyng <tduyng.dev@gmail.com>
2025-04-20 22:09:01 +0200

    feat(gozzi): update CI, scripts to build with gozzi

--- build.sh
+++ build.sh
@@ -5,17 +5,17 @@ OUTPUT_CONFIG="config.toml"
 
 # Determine environment
 if [ "$1" == "production" ]; then
-    ENV_CONFIG="config/config.production.toml"
-    echo "Using production config"
+  ENV_CONFIG="config/config.production.toml"
+  echo "Using production config"
 else
-    ENV_CONFIG="config/config.dev.toml"
-    echo "Using development config"
+  ENV_CONFIG="config/config.dev.toml"
+  echo "Using development config"
 fi
 
 # Merge configurations
-cat "$BASE_CONFIG" > "$OUTPUT_CONFIG" # Copy base config to output config
-echo "" >> "$OUTPUT_CONFIG"           # Add a newline to output config
-cat "$ENV_CONFIG" >> "$OUTPUT_CONFIG" # Append environment config to output config
+cat "$BASE_CONFIG" >"$OUTPUT_CONFIG" # Copy base config to output config
+echo "" >>"$OUTPUT_CONFIG"           # Add a newline to output config
+cat "$ENV_CONFIG" >>"$OUTPUT_CONFIG" # Append environment config to output config
 
 # Build the site
-zola build
+gozzi build
